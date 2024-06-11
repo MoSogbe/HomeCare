dc290010285a0f8fac3516aabafbceb9248f4364
mosogbe <mohammed.sogbe@yahoo.com>
2024-06-11 09:35:29 +0000

    datbase change

--- models/daily_notes.py
+++ models/daily_notes.py
@@ -8,7 +8,7 @@ class DailyNotesModel(db.Model):
     participant = db.relationship('ParticipantModel', backref='daily_notes')
     created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     user = db.relationship('UserModel', backref='daily_notes')
-    reviewed_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    reviewed_by = db.Column(db.String(11))
     comment = db.Column(db.String(425),nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False)
     updated_at = db.Column(db.DateTime(), nullable=False)
