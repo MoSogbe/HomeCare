b81cfcc5790d0674519e839bde99c6d8b7ee5860
mosogbe <mohammed.sogbe@yahoo.com>
2024-07-14 14:45:06 +0000

    in and out logs

--- schema/log_entry.py
+++ schema/log_entry.py
@@ -6,6 +6,7 @@ class LogEntrySchema(Schema):
     participant_id = fields.Int(required=True)
     user_id = fields.Int(required=True)
     check_in = fields.DateTime(required=True)
+    check_out = fields.DateTime()
     notes = fields.Str()
     service_id = fields.Int()
     created_at = fields.DateTime(dump_only=True)
