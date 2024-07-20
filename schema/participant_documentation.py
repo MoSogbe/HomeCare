9edb70a8900f03d466136af74f887a75e1b018c1
mosogbe <mohammed.sogbe@yahoo.com>
2024-07-20 16:11:42 +0000

    documentation update

--- schema/participant_documentation.py
+++ schema/participant_documentation.py
@@ -5,6 +5,6 @@ class ParticipantDocumentationSchema(Schema):
     participant_id = fields.Int(required=True)
     created_by = fields.Int(dump_only=True)
     document_type_id = fields.Int(required=True)
-    file_path = fields.Str(dump_only=True)
+    file_path = fields.Str(required=True)
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
